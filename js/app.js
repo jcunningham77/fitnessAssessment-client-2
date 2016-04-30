5f6a317a90bb64e89de30df9d49158178c93f697
Jeffrey Cunningham <jcunningham77@gmail.com>
2016-04-30 14:44:15 -0400

    call backendless for the users

--- js/app.js
+++ js/app.js
@@ -17,8 +17,8 @@ angular.module('fitnessAssessment-client-2',['ngRoute'])
      $scope.params = $routeParams;
      dataService.getUsers(function(response){
 		// debugger;
-		console.log("data from the response = " + response.data);	
-		$scope.users = response.data;
+		console.log("data from the response = " + JSON.stringify(response.data));	
+		$scope.users = response.data.data;
 	});
 
      	$scope.deleteUser = function(user, $index){
@@ -50,7 +50,24 @@ angular.module('fitnessAssessment-client-2',['ngRoute'])
 
 	this.getUsers = function(callback){
 		console.log("in the dataService getUsers method");
-		$http.get('mock/users.json')
-		.then(callback)
+		// $http.get('mock/users.json')
+		// .then(callback)
+            var config = {
+                headers : {
+                    'authorization': undefined,
+                    'application-id': 'CCD64643-E1D9-9AA2-FFF6-93992E5B9D00',
+                    'secret-key':'266A1786-4FFA-FBAE-FFD7-53D4EEF7A700',
+                    'application-type':'REST',
+                    'Content-Type':'application/json'
+                }
+            }
+            $http.get('https://api.backendless.com/v1/data/Users?props=firstName,lastName,objectId', config).then(callback, handleError);
 	}
+
+    function handleError(res) {
+	    // debugger;
+	    console.log("the error was = " + res.data.message);
+	   
+	    return { success: false, message: res.data.message};
+    }
   });
